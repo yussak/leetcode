ea603941e9fa6c040f86f7658557ce7a2689179b
YusukeSakuraba <69101210+YusukeSakuraba@users.noreply.github.com>
2023-08-01 10:56:29 +0900

    Update 217-contains-duplicate.js

--- easy/217-contains-duplicate.js
+++ easy/217-contains-duplicate.js
@@ -9,17 +9,19 @@ var containsDuplicate = function(nums) {
     // console.log(nums)
     // console.log(new Set(nums).size)
 
-    // const nl=nums.length
-    // const ss=new Set(nums).size
-
-    // // if (nl !== ss) {
-    // //     return true
-    // // }else {
-    // //     return false
-    // // }
-    // console.log("nl,ns",nl!==ss)
-    // console.log("nl,ns",nl===ss)
-
     return nums.length!==new Set(nums).size
-
 };
+
+// ポイント
+
+// 以下はもっとスマートに書ける
+// if (nl !== ss) {
+//     return true
+// }else {
+//     return false
+// }
+
+// こう書ける
+// return nums.length!==new Set(nums).size
+
+// !==なのは、両方が一致しない時にtrueにするため
