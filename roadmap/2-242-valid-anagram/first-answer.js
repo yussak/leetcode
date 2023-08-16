09a0504274d168d5b7fbd8854ed99c654d6f160a
YusukeSakuraba <69101210+YusukeSakuraba@users.noreply.github.com>
2023-08-16 13:42:08 +0900

    Update first-answer.js

--- roadmap/2-242-valid-anagram/first-answer.js
+++ roadmap/2-242-valid-anagram/first-answer.js
@@ -1,9 +1,16 @@
+// https://leetcode.com/problems/valid-anagram/submissions/
+// Runtime
+// 82ms
+// Beats 55.07%of users with JavaScript
+// Memory
+// 48.12mb
+// Beats 23.18%of users with JavaScript
+
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    // これだとパフォーマンスが良くない気がするので、もっといい答えを考える
     return s.split("").sort().join("")===t.split("").sort().join("")
 };
