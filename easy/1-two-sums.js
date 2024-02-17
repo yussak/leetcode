9900b13e2827ecabd2b5a65c0aaf3a6f59a42828
yussak <69101210+yussak@users.noreply.github.com>
2024-02-17 14:07:54 +0900

    Update 1-two-sums.js

--- easy/1-two-sums.js
+++ easy/1-two-sums.js
@@ -1,4 +1,24 @@
-
+// 模範（chatgptに聞いた）
+// Runtime 62ms Beats 66.40% of users with JavaScript
+// Memory 51.42MB Beats 12.61% of users with JavaScript
+var twoSum = function(nums, target) {
+    // hashmapを使うとfor一回に減らせる
+    const map = new Map();
+    
+    // ９−2=7で7は存在するので[0,1], 6-3=3=>3は他には存在しないので何もしない、6-2=4=4は存在するので[]1,2]
+    // target-nums[i]の要素が存在しているかチェックし、あるなら[i, target-nums[i]のindex]が答えになる
+    
+    for (let i　=0; i<nums.length; i++) {
+        const complement = target-nums[i];
+        if (map.has(complement)) {
+            // 答えは一つだけだからtrueの時点でreturnする
+            return [i, map.get(complement)];
+        }
+        // ifチェックしてからsetの理由：すでにmapにある値を追加しないため
+        // キーがnums[i]でバリューがiの理由：今回必要なのはindexだから
+        map.set(nums[i], i);
+    }
+};
 
 // brute-forceの解答
 // Runtime 105ms Beats 29.74% of users with JavaScript
