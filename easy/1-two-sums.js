99d002f89dcc1a50f5ca1573682b20eb4596f32e
yussak <69101210+yussak@users.noreply.github.com>
2024-02-17 13:29:36 +0900

    Update 1-two-sums.js

--- easy/1-two-sums.js
+++ easy/1-two-sums.js
@@ -1,6 +1,9 @@
 
 
 // brute-forceの解答
+// Runtime 105ms Beats 29.74% of users with JavaScript
+// Memory 49.43MB Beats 36.54% of users with JavaScript
+
 // 以下だと計算量がO(N^2）で非効率
 // j=i+1としているので同じ計算を何回もすることはなくなる
 var twoSum = function(nums, target) {
